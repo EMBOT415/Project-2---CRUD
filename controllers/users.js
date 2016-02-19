4aa437a321afea2afb629e9f95bece5da5a18555
EMBOT415 <embot415@gmail.com>
2016-02-19 12:26:42 -0500

    user display

--- controllers/users.js
+++ controllers/users.js
@@ -5,9 +5,26 @@ var express 		= require('express'),
 
 
 router.get('/', function(req, res){
-	res.render('users/users.ejs');
+	//res.render('users/users.ejs');
 	//res.send('sup');
+
+	User.find({}, function(err, data){
+		res.render('users/users.ejs', {
+			users: data
+		});
+	})
 });
+
+router.get('/new', function(req, res) {
+	res.render('users/new.ejs');
+})
+
+router.post('/', function(req, res) {
+	var newUser = new User(req.body);
+	newUser.save(function(err, data) {
+		res.redirect('/');
+	})
+})
 
 
 module.exports = router;
--- models/user1.js
+++ models/user1.js
@@ -1,12 +1,11 @@
 var mongoose 		= require('mongoose');
 
 var userSchema = mongoose.Schema({
-	name: String,
+	username: String,
 	email: String,
 	password: String
 
 })
 
-var User = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
 
-module.exports = User;
