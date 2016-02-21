bb3e3a52206b5792bdbf8b2f9dd97d885c77053b
EMBOT415 <embot415@gmail.com>
2016-02-21 15:33:27 -0500

    index page looks ok

--- public/js/index.js
+++ public/js/index.js
@@ -4,14 +4,18 @@ window.onload = function() {
 
 
 
-var $clientlogin = $('#enter').click(function(){
-    //$('#blank').hide();
-    $('#client').show();
-});
 
 
+// $(".wrap > div:gt(0)").hide();
 
-
+// setInterval(function() { 
+//   $('.wrap > div:first')
+//     .fadeOut(8000)
+//     .next()
+//     .fadeIn(8000)
+//     .end()
+//     .appendTo('.wrap');
+// },  8000);
 
 
 
